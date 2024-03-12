cca867a5fbce407aa5cc1a601f915714e53f6246
hck219 <1851515964@qq.com>
2024-03-12 14:50:15 +0800

    *

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function imageSlider() {
+  let imageBox = document.querySelector('.image-slider-container');
+  let i = 0;
+
+  setInterval(function () {
+    imageBox.style.left = -i * 1920 + 'px';
+
+    i = (i + 1) % 5;
+  }, 3000);
+}
+
+imageSlider();
